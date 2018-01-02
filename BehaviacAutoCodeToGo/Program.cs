da9b4bc8e1c0d1ca53b0c63af40e1bba74376e87
leviyuan <450446856@qq.com>
2018-01-02 16:02:11 +0800

    - mod 添加安全检查代码

--- BehaviacAutoCodeToGo/Program.cs
+++ BehaviacAutoCodeToGo/Program.cs
@@ -15,12 +15,22 @@ namespace behaviac_autoCodeToGo
         static string GoBridgePath = "G:/wproject_pokemon/trunk/Server/src/behaviac/";
         static string GoImportPath = "behaviac";
 
+        static Dictionary<string, string> typeErrorValue = new Dictionary<string, string>();
+
         static void Main(string[] args)
         {
             if (args.Length > 0) MetaPath = args[0];
             if (args.Length > 1) CppExportPath = args[1] + "/";
             if (args.Length > 2) GoBridgePath = args[2] + "/";
             if (args.Length > 3) GoImportPath = args[3];
+
+            typeErrorValue.Add("string", "\"\"");
+            typeErrorValue.Add("const char*", "\"\"");
+            typeErrorValue.Add("bool", "false");
+            typeErrorValue.Add("int", "0");
+            typeErrorValue.Add("float64", "0");
+            typeErrorValue.Add("double", "0");
+            typeErrorValue.Add("", "");
 
             try
             {
@@ -128,6 +138,10 @@ namespace behaviac_autoCodeToGo
                         // get
                         appendCodeLine("EXPORT " + codeType + " Agent_" + className + "_Get" + memberNode.Attributes["Name"].InnerText + "(int agentid) {");
                         appendCodeLine(className + "* agent = (" + className + "*)allAgentInstances[agentid];");
+                        appendCodeLine("if (agent == NULL){");
+                        appendCodeLine("LogManager::GetInstance()->Log(\"agent has been destroyed, id = %i\", agentid);");
+                        appendCodeLine("return " + (kind == TypeKind.Agent ? "-1" : typeErrorValue[codeType]) + ";");
+                        appendCodeLine("}");
                         if (kind == TypeKind.Agent)
                             appendCodeLine("return agent->" + memberNode.Attributes["Name"].InnerText + " == nullptr ? -1 : agent->" + memberNode.Attributes["Name"].InnerText + "->GetId();");
                         else
@@ -136,6 +150,10 @@ namespace behaviac_autoCodeToGo
                         // set
                         appendCodeLine("EXPORT void Agent_" + className + "_Set" + memberNode.Attributes["Name"].InnerText + "(int agentid, " + codeType + " value) {");
                         appendCodeLine(className + "* agent = (" + className + "*)allAgentInstances[agentid];");
+                        appendCodeLine("if (agent == NULL){");
+                        appendCodeLine("LogManager::GetInstance()->Log(\"agent has been destroyed, id = %i\", agentid);");
+                        appendCodeLine("return;");
+                        appendCodeLine("}");
                         if (kind == TypeKind.Agent)
                             appendCodeLine("agent->" + memberNode.Attributes["Name"].InnerText + " = (" + memberNode.Attributes["Type"].InnerText + ")allAgentInstances[value];");
                         else
@@ -232,6 +250,9 @@ namespace behaviac_autoCodeToGo
                     StringBuilder gosb = new StringBuilder();
                     gosb.AppendLine("package behaviac");
                     gosb.AppendLine("import \"" + GoImportPath + "/internal\"");
+                    gosb.AppendLine("import \"github.com/name5566/leaf/log\"");
+                    gosb.AppendLine("import \"runtime/debug\"");
+
                     // 重写Funcs的极口
                     gosb.AppendLine("type Agent" + className + "Overwrite interface {");
                     funcs_ptr.Split(';').ToList<string>().ForEach(code =>
@@ -282,9 +303,17 @@ namespace behaviac_autoCodeToGo
                         codeType = codeType == "float" ? "float32" : (codeType == "double" ? "float64" : codeType);
                         string goName = memberNode.Attributes["Name"].InnerText.Substring(0, 1).ToUpper() + memberNode.Attributes["Name"].InnerText.Substring(1);
                         gosb.AppendLine("func (ins *Agent" + className + ") Get" + goName + "() " + codeType + "{");
+                        gosb.AppendLine("if ins.m_id < 0 {");
+                        gosb.AppendLine("log.Error(\"agent has been destroyed %s\", debug.Stack())");
+                        gosb.AppendLine("return " + typeErrorValue[codeType]);
+                        gosb.AppendLine("}");
                         gosb.AppendLine("return internal.Agent_" + className + "_Get" + memberNode.Attributes["Name"].InnerText + "(ins.m_id)");
                         gosb.AppendLine("}");
                         gosb.AppendLine("func (ins *Agent" + className + ") Set" + goName + "(value " + codeType + " ) {");
+                        gosb.AppendLine("if ins.m_id < 0 {");
+                        gosb.AppendLine("log.Error(\"agent has been destroyed %s\", debug.Stack())");
+                        gosb.AppendLine("return");
+                        gosb.AppendLine("}");
                         gosb.AppendLine("internal.Agent_" + className + "_Set" + memberNode.Attributes["Name"].InnerText + "(ins.m_id, value)");
                         gosb.AppendLine("}");
                     }
@@ -307,6 +336,10 @@ namespace behaviac_autoCodeToGo
                         codeType = codeType == "void" ? "" : codeType;
                         codeType = codeType == "float" ? "float32" : (codeType == "double" ? "float64" : codeType);
                         gosb.AppendLine("func (ins *Agent" + className + ") " + methodName.Substring(0, 1).ToUpper() + methodName.Substring(1) + "(" + paramTypeStr + ") " + codeType + " {");
+                        gosb.AppendLine("if ins.m_id < 0 {");
+                        gosb.AppendLine("log.Error(\"agent has been destroyed %s\", debug.Stack())");
+                        gosb.AppendLine("return " + typeErrorValue[codeType]);
+                        gosb.AppendLine("}");
                         gosb.AppendLine((string.IsNullOrEmpty(codeType) ? "" : "return ") + "agent" + className + "OverwriteIns.Agent" + className + "_" + methodName + "(ins.m_id" + paramCallStr + ")");
                         gosb.AppendLine("}");
                     }
